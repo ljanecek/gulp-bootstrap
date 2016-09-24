620197a1ca82b6c88b90b2c777c80192dec5f810
Ladislav Janeček <ladislavjanecek@gmail.com>
2016-09-24 15:45:02 +0200

    comments

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,39 @@
 var env = process.env.NODE_ENV || 'local';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var babel = require('babelify');
+var gulpif = require('gulp-if');
+var pixrem = require('gulp-pixrem');
+var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
+var minifycss = require('gulp-clean-css');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var inlineimage = require('gulp-inline-image');
+var autoprefixer = require('gulp-autoprefixer');
+var moduleImporter = require('sass-module-importer');
+
+
+//
+// Paths
+// ------------------------------
+
+var paths = {
+    src: {
+        styles: './src/scss/application.scss',
+        scripts: './src/js/application.js'
+    },
+    watch: {
+        styles: './src/scss/**/*.scss',
+        scripts: './src/js/**/*.js'
+    },
+    dist: {
+        styles: './dist/css',
+        scripts: './dist/js'
+    }
+};
 
 
 //
@@ -16,7 +45,8 @@ var source = require('vinyl-source-stream');
 // ------------------------------
 
 gulp.task('watch', function() {
-    gulp.watch('src/js/**/*.*', { interval: 500 }, ['scripts']);
+    gulp.watch(paths.watch.scripts, { interval: 500 }, ['scripts']);
+	gulp.watch(paths.watch.styles, { interval: 500 }, ['styles']);
 });
 
 
@@ -24,9 +54,33 @@ gulp.task('watch', function() {
 // Tasks
 // ------------------------------
 
+
+gulp.task('styles', function() {
+    gulp.src(paths.src.styles)
+        .pipe(gulpif(env == 'local', sourcemaps.init()))
+        .pipe(sass({
+            importer: moduleImporter()
+        }).on('error', sass.logError))
+		.pipe(pixrem({
+			rootValue: '62.5%',
+			atrules: true
+	    }))
+        .pipe(autoprefixer({
+            browsers: ['> 2%', 'IE 8'],
+            cascade: false
+        }))
+        .pipe(inlineimage())
+        .pipe(gulpif(env != 'local', minifycss({
+            compatibility: 'ie8'
+        })))
+    	.pipe(gulpif(env == 'local', sourcemaps.write()))
+        .pipe(gulp.dest(paths.dist.styles));
+});
+
+
 gulp.task('scripts', function() {
 
-    browserify('./src/js/application.js', {
+    browserify(paths.src.scripts, {
             debug: true
         })
 		/*
@@ -46,6 +100,15 @@ gulp.task('scripts', function() {
             console.error(err);
             this.emit('end');
         })
+		.on('error', function(e) {
+            console.error(e);
+            this.emit('end');
+        })
+        .pipe(gulpif(env != 'local', uglify()))
+        .on('error', function(e) {
+            console.error(e);
+            this.emit('end');
+        })
         .pipe(gulp.dest('./dist/js'));
 
 });
@@ -55,4 +118,4 @@ gulp.task('scripts', function() {
 // Default task & Run
 // ------------------------------
 
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'scripts', 'styles']);
